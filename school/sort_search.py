819a38998f5653f5e9d841c6a5183760ce10f416
Szabon Benjamin <107744884+Benjamin-Szabon@users.noreply.github.com>
2024-05-28 08:22:35 +0200

    bugfix sort_search.py

--- school/sort_search.py
+++ school/sort_search.py
@@ -17,7 +17,7 @@ def insertion_sort(list):
 
 def binary_search(lista):
     kis = 0
-    nagy = len(lista)
+    nagy = len(lista)-1
     keresett = int(input("Melyik számot keresed? "))
     megtalalva = False
 
