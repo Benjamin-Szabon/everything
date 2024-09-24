66b248d196e6ff23d157d4166999c421be3b8d00
Szabon Benjamin <szabon.benjamin@gmail.com>
2024-09-24 16:39:58 +0200

    Update gyumolcs.py

--- OKTV_gyakorlo/gyumolcs.py
+++ OKTV_gyakorlo/gyumolcs.py
@@ -1,34 +1,45 @@
-ladak_szama = int(input("Ládák száma: "))
-ladak_tartalma = ""
-felpakolt = 0
-valid = False
-s = ladak_tartalma[0]
-half = ladak_szama / 2
-i = 0
+import random
 
-def input(ladak_tartalma):
-    while valid == False:
-        valid = True
-        ladak_tartalma = input("Ládák tartalma (A/K): ")
+#ládak szama (2<N<100 000, N páros szám))
+n = 100
+ladak_szama = n
 
-        #ellenőrzi hogy az adatot hogy lehet-e vele dolgozni
-        if len(ladak_tartalma) == ladak_szama:
-            for betu in ladak_tartalma:
-                if betu.upper() not in ["A", "K"]:
-                    valid = False
-                    break
+#ládák tartalma
+ladak_tartalma = ['A'] * int(n/2) + ['K'] * int(n/2)
+random.shuffle(ladak_tartalma)
+#print(ladak_tartalma)
+
+kozep = ladak_szama / 2
+arany = 1
 
 
-def szamlalo(felpakolt):
+#ismétlés ameddig nem lesz egyenlő az arány
+while arany != 0:
+    #értékek nullázása
+    arany = 0
+    ertek = 1
     felpakolt = 0
-    for lada in ladak_tartalma:
-        if lada == s:
+    keresett = "A"
+    i = 0
+
+    while i < ladak_szama:
+        #'kozep'-et elérve a 'keresett' és 'ertek' kicserélése
+        if i == kozep:
+            keresett = "K"
+            ertek = -1
+        #ládák számolása
+        if ladak_tartalma[i] == keresett:
             felpakolt += 1
-            s = lada
-     
+            arany += ertek
+        
+        i += 1
 
-while half > i:
-    input()
-    szamlalo(felpakolt)
+    #'kozep' változtatása
+    if arany > 0:
+        kozep = (kozep - ladak_szama) / 2
+    elif arany < 0:
+        kozep = (kozep + ladak_szama) / 2
 
+#kiíratások
+#print("arány:", arany)
 print(f"Kamionra felpakolt ládák száma: {felpakolt}")
